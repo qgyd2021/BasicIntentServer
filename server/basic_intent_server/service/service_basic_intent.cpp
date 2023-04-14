189676c7cc2cb4531a62c7ef123890c0f0a2ae6a
晴耕雨读 <qgyd2021@gmail.com>
2023-04-14 11:56:59 +0800

    20230414094200

--- server/basic_intent_server/service/service_basic_intent.cpp
+++ server/basic_intent_server/service/service_basic_intent.cpp
@@ -60,26 +60,26 @@ ModelGroup::~ModelGroup() {
 
 BasicIntentService::BasicIntentService()
 {
-  nlohmann::json models_json;
+  //nlohmann::json models_json;
   std::cout << "FLAGS_models_json" << std::endl;
-  std::cout << FLAGS_models_json << std::endl;
-  exit(0);
-  std::ifstream i_models_json(FLAGS_models_json);
-  if (! i_models_json.is_open()) {
-    std::cout << "fail to open file\n" << FLAGS_models_json;
-    exit(0);
-  }
-  i_models_json >> models_json;
-  for (const auto & model_json:models_json) {
-    std::string key = model_json["key"];
-    std::string model_path = model_json["model_path"];
-    std::string vocab_file = model_json["vocab_file"];
-
-    std::string model_file = model_path + "/model.pth";
-    std::string labels_file = model_path + "/labels.json";
-
-    this->key_to_model_group_map_[key] = new ModelGroup(model_file, labels_file, vocab_file);
-  }
+  //std::cout << FLAGS_models_json << std::endl;
+  //exit(0);
+  //std::ifstream i_models_json(FLAGS_models_json);
+  //if (! i_models_json.is_open()) {
+  //  std::cout << "fail to open file\n" << FLAGS_models_json;
+  //  exit(0);
+  //}
+  //i_models_json >> models_json;
+  //for (const auto & model_json:models_json) {
+  //  std::string key = model_json["key"];
+  //  std::string model_path = model_json["model_path"];
+  //  std::string vocab_file = model_json["vocab_file"];
+  //
+  //  std::string model_file = model_path + "/model.pth";
+  //  std::string labels_file = model_path + "/labels.json";
+  //
+  //  this->key_to_model_group_map_[key] = new ModelGroup(model_file, labels_file, vocab_file);
+  //}
 };
 
 
--- server/basic_intent_server/settings.cpp
+++ server/basic_intent_server/settings.cpp
@@ -1,8 +1,6 @@
 //
 // Created by tianx on 2023/4/14.
 //
-#include <iostream>
-
 #include <gflags/gflags.h>
 
 DEFINE_string(project_name, "BasicIntentServer", "a string to indicate the name of the server");
@@ -10,7 +8,3 @@ DEFINE_string(http_host, "0.0.0.0", "server host");
 DEFINE_uint32(http_port, 80, "server port");
 
 DEFINE_string(models_json, "server/basic_intent_server/config/models.json", "models_json");
-
-
-std::cout << "FLAGS_models_json" << std::endl;
-std::cout << FLAGS_models_json << std::endl;
