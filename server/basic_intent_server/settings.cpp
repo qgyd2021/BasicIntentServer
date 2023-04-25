f9883cd0e006aab588192487d5575ae60ad85cf2
晴耕雨读 <qgyd2021@gmail.com>
2023-04-25 18:30:36 +0800

    20230425183000

--- server/basic_intent_server/settings.cpp
+++ server/basic_intent_server/settings.cpp
@@ -7,7 +7,7 @@ DEFINE_string(project_name, "BasicIntentServer", "a string to indicate the name 
 DEFINE_string(http_host, "0.0.0.0", "server host");
 DEFINE_uint32(http_port, 80, "server port");
 
-DEFINE_string(models_json, "server/basic_intent_server/config/models.json", "models_json");
+DEFINE_string(models_json, "server/basic_intent_server/json_config/models.json", "models_json");
 
 DEFINE_string(basic_intent_log_dir, "./logs", "basic intent log dir");
 DEFINE_uint32(basic_intent_stderrthreshold, 0, "basic intent stderr threshold");
